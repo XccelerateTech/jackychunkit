3d9c698d05ccebfa5bf54cfba29cbe72e39f26fe
Jackykit123 <jackykit19970402@gmail.com>
2018-11-01 00:39:08 +0800

    translated all previous old exercism to new ES6 version and redone all preivous finished exercism

---
+++ Exercism/javascript/list-ops/list-ops.js
@@ -0,0 +1,55 @@
+export default class List {
+    constructor(input) {
+        if (typeof input == 'undefined') {
+            this.values = [];
+        } else {
+            this.values = input;
+        }
+    }
+    length() {
+        return this.values.length;
+    }
+    append(input) {
+        var output = this.values;
+        for (var i = 0; i < input.values.length; i++) {
+            output.push(input.values[i])
+        }
+        return new List(output);
+    }
+    concat(input) {
+        return this.append(input);
+    }
+    filter(input) {
+        var output = new Array;
+        for (var i = 0; i < this.values.length; i++) {
+            if (input(this.values[i])) {
+                output.push(this.values[i])
+            }
+        }
+        return new List(output);
+    }
+    map(operation) {
+        var output = new Array;
+        for (var i = 0; i < this.values.length; i++) {
+            output.push(operation(this.values[i]));
+        }
+        return new List(output);
+    }
+    foldl(operation, value) {
+        var accumulatedValue = value;
+        for (var i = 0; i < this.values.length; i++) {
+            accumulatedValue = operation(accumulatedValue, this.values[i]);
+        }
+        return accumulatedValue;
+    }
+    foldr(operation, value) {
+        return this.reverse().foldl(operation, value);
+    }
+    reverse() {
+        var output = new Array;
+        for (var i = 0; i < this.values.length; i++) {
+            output.unshift(this.values[i]);
+        }
+        return new List(output);
+    }
+}
