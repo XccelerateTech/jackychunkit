9f55104f86c07add7e73ed487fa713dd6794ec41
Jackykit123 <jackykit19970402@gmail.com>
2018-11-01 16:36:06 +0800

    updated answer

--- Exercism/javascript/list-ops/list-ops.js
+++ Exercism/javascript/list-ops/list-ops.js
@@ -7,49 +7,48 @@ export default class List {
         }
     }
     length() {
-        return this.values.length;
+        let length = 0;
+        for (let i = 0; this.values[i]; i++) {
+            length++;
+        }
+        return length;
     }
     append(input) {
-        var output = this.values;
-        for (var i = 0; i < input.values.length; i++) {
-            output.push(input.values[i])
-        }
-        return new List(output);
+        this.values = [...this.values, ...input.values]
+        return this;
     }
     concat(input) {
         return this.append(input);
     }
     filter(input) {
-        var output = new Array;
-        for (var i = 0; i < this.values.length; i++) {
+        var output = [];
+        for (var i = 0; i < this.length(); i++) {
             if (input(this.values[i])) {
-                output.push(this.values[i])
+                output = [...output,this.values[i]]
             }
-        }
-        return new List(output);
+        } return new List(output);
     }
     map(operation) {
-        var output = new Array;
-        for (var i = 0; i < this.values.length; i++) {
-            output.push(operation(this.values[i]));
+        var output = [];
+        for (var i = 0; i < this.length(); i++) {
+            output = [...output,(operation(this.values[i]))];
         }
-        return new List(output);
+        this.values = output;
+        return this;
     }
     foldl(operation, value) {
         var accumulatedValue = value;
-        for (var i = 0; i < this.values.length; i++) {
+        for (var i = 0; i < this.length(); i++) {
             accumulatedValue = operation(accumulatedValue, this.values[i]);
-        }
-        return accumulatedValue;
+        } return accumulatedValue;
     }
     foldr(operation, value) {
         return this.reverse().foldl(operation, value);
     }
     reverse() {
-        var output = new Array;
-        for (var i = 0; i < this.values.length; i++) {
-            output.unshift(this.values[i]);
-        }
-        return new List(output);
+        var output = [];
+        for (var i = 0; i < this.length(); i++) {
+            output = [this.values[i], ...output];
+        } return new List(output);
     }
 }
