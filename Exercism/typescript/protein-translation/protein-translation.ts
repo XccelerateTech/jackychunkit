55eaaea9bb5059c44750cf6c8664edd33d553c03
Jackykit123 <jackykit19970402@gmail.com>
2018-12-17 21:39:02 +0800

    started typescript javascript track

---
+++ Exercism/typescript/protein-translation/protein-translation.ts
@@ -0,0 +1,59 @@
+class ProteinTranslation {
+    static proteins(rna: string) {
+        const orginalRna: string = rna;
+        let protein: string[] = [];
+        let codon: string;
+        while (rna) {
+            codon = rna.slice(0, 3);
+            switch (codon) {
+                case 'AUG':
+                    protein.push('Methionine');
+                    break;
+
+                case 'UUU':
+                case 'UUC':
+                    protein.push('Phenylalanine');
+                    break;
+
+                case 'UUA':
+                case 'UUG':
+                    protein.push('Leucine');
+                    break;
+
+                case 'UCU':
+                case 'UCC':
+                case 'UCA':
+                case 'UCG':
+                    protein.push('Serine');
+                    break;
+
+                case 'UAU':
+                case 'UAC':
+                    protein.push('Tyrosine');
+                    break;
+
+                case 'UGU':
+                case 'UGC':
+                    protein.push('Cysteine');
+                    break;
+
+                case 'UGG':
+                    protein.push('Tryptophan');
+                    break;
+
+                case 'UAA':
+                case 'UAG':
+                case 'UGA':
+                    rna = '';
+                    break;
+
+                default:
+                    throw `Invalid RNA '${orginalRna}', codon '${codon}' is invalid.`
+            }
+            rna = rna.slice(3)
+        }
+        return protein;
+    }
+}
+
+export default ProteinTranslation
