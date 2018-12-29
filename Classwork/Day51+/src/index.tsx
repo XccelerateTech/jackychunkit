b245ef9e83f50ff5858b594317ce3a2ef3103baa
Jackykit123 <jackykit19970402@gmail.com>
2018-12-29 22:22:40 +0800

    moved into folders

--- Classwork/Day51+/src/index.tsx
+++ Classwork/Day51+/src/index.tsx
@@ -4,26 +4,27 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 // =================Day 51=================
-// import App from './Day51_App';
+// import App from './Day51/App';
 // ReactDOM.render(
 //   <App />,
 //   document.getElementById('root') as HTMLElement
 // );
 
 
-// =================Day 52 Comment Card=================
-// import App from './Day52_CommentCard_App';
+// // =================Day 52 Comment Card=================
+// import App from './Day52/CommentCard/App';
 // const list = [{ id: 1, name: 'Sam', comment: `it's good` }, { id: 2, name: 'Altaf', comment: `it's bad` }]
 // ReactDOM.render(
 //   <App list={list} />,
 //   document.getElementById('root') as HTMLElement
 // );
-// registerServiceWorker();
 
 // =================Day 52 Questioner=================
-import Questioner from './Day52_Questioner_App';
+import Questioner from './Day52/Questioner/App';
 ReactDOM.render(
   <Questioner question="what is your name" />,
   document.getElementById('root') as HTMLElement
 )
+
+
 registerServiceWorker();
