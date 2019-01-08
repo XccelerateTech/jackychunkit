76f5c81b54f6927be28e0a9320c1a1feed665ff6
Jackykit123 <jackykit19970402@gmail.com>
2019-01-08 21:42:17 +0800

    edited day59

--- Classwork/Day51+/react/src/Day59/Default/App.tsx
+++ Classwork/Day51+/react/src/Day59/Default/App.tsx
@@ -10,19 +10,9 @@ class App extends React.Component {
         <header>
           <h1>Simple Website</h1>
         </header>
-        <section>
-          This is a simple website made without React. Try to convert this into React enabled.
-            <ol>
-            <li>First, you need to use <span className="command">create-react-app</span></li>
-            <li>Second, you need to run <span className="command">npm start</span></li>
-          </ol>
-        </section>
-        <footer>
-          <img src={logo} />
-        </footer>
-      </>
-    );
-  }
+     </div>
+   );
+ }
 }
 
 export default App;
--- Classwork/Day51+/react/src/index.test.tsx
+++ Classwork/Day51+/react/src/index.test.tsx
@@ -1,5 +1,5 @@
 import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import * as Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
