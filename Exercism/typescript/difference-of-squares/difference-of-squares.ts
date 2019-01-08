af34e224a10733f07f52e7e7a050ba42cd4db672
Jackykit123 <jackykit19970402@gmail.com>
2019-01-08 18:16:09 +0800

    modified tslint error

--- Exercism/typescript/difference-of-squares/difference-of-squares.ts
+++ Exercism/typescript/difference-of-squares/difference-of-squares.ts
@@ -1,14 +1,14 @@
 export default class Sqaures {
-    private naturalNumber: number = 0;
-    public squareOfSum: number = 0;
-    public sumOfSquares: number = 0;
-    public difference: number = 0;
+    private naturalNumber: number = 0
+    public squareOfSum: number = 0
+    public sumOfSquares: number = 0
+    public difference: number = 0
     constructor(input: number) {
-        this.naturalNumber = input;
-        this.squareOfSum = Math.pow(((1 + this.naturalNumber) * this.naturalNumber / 2), 2);
-        for (let i:number = 0; i <= this.naturalNumber; i++) {
-            this.sumOfSquares += i * i;
+        this.naturalNumber = input
+        this.squareOfSum = Math.pow(((1 + this.naturalNumber) * this.naturalNumber / 2), 2)
+        for (let i: number = 0; i <= this.naturalNumber; i++) {
+            this.sumOfSquares += i * i
         }
-        this.difference = this.squareOfSum - this.sumOfSquares;
+        this.difference = this.squareOfSum - this.sumOfSquares
     }
 }
--- Exercism/typescript/protein-translation/protein-translation.ts
+++ Exercism/typescript/protein-translation/protein-translation.ts
@@ -1,57 +1,57 @@
 export default class ProteinTranslation {
     public static proteins(rna: string) {
-        const orginalRna: string = rna;
-        let protein: string[] = [];
-        let codon: string;
+        const orginalRna = rna
+        const protein: string[] = []
+        let codon: string
         while (rna) {
-            codon = rna.slice(0, 3);
+            codon = rna.slice(0, 3)
             switch (codon) {
                 case 'AUG':
-                    protein.push('Methionine');
-                    break;
+                    protein.push('Methionine')
+                    break
 
                 case 'UUU':
                 case 'UUC':
-                    protein.push('Phenylalanine');
-                    break;
+                    protein.push('Phenylalanine')
+                    break
 
                 case 'UUA':
                 case 'UUG':
-                    protein.push('Leucine');
-                    break;
+                    protein.push('Leucine')
+                    break
 
                 case 'UCU':
                 case 'UCC':
                 case 'UCA':
                 case 'UCG':
-                    protein.push('Serine');
-                    break;
+                    protein.push('Serine')
+                    break
 
                 case 'UAU':
                 case 'UAC':
-                    protein.push('Tyrosine');
-                    break;
+                    protein.push('Tyrosine')
+                    break
 
                 case 'UGU':
                 case 'UGC':
-                    protein.push('Cysteine');
-                    break;
+                    protein.push('Cysteine')
+                    break
 
                 case 'UGG':
-                    protein.push('Tryptophan');
-                    break;
+                    protein.push('Tryptophan')
+                    break
 
                 case 'UAA':
                 case 'UAG':
                 case 'UGA':
-                    rna = '';
-                    break;
+                    rna = ''
+                    break
 
                 default:
                     throw `Invalid RNA '${orginalRna}', codon '${codon}' is invalid.`
             }
             rna = rna.slice(3)
         }
-        return protein;
+        return protein
     }
 }
