97db1c036515457e561e8192b347468e452462da
Jackykit123 <jackykit19970402@gmail.com>
2019-01-08 21:47:14 +0800

    edited Day59

--- Classwork/Day51+/react/src/Day59/Default/App.tsx
+++ Classwork/Day51+/react/src/Day59/Default/App.tsx
@@ -10,9 +10,19 @@ class App extends React.Component {
         <header>
           <h1>Simple Website</h1>
         </header>
-     </div>
-   );
- }
+        <section>
+          This is a simple website made without React. Try to convert this into React enabled.
+            <ol>
+            <li>First, you need to use <span className="command">create-react-app</span></li>
+            <li>Second, you need to run <span className="command">npm start</span></li>
+          </ol>
+        </section>
+        <footer>
+          <img src={logo} />
+        </footer>
+      </>
+    );
+  }
 }
 
 export default App;
